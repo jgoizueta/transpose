dfe15b1ceac9b1336874cc731dca2db178451c40
Javier Goizueta <jgoizueta@gmail.com>
2015-12-25 13:50:39 +0100

    Better support for alternate-bass chords

--- lib/chord.rb
+++ lib/chord.rb
@@ -1,5 +1,5 @@
 class Chord
-  PATTERN = /\b([A-G])(b|#)?((?:maj|min|[Mm+°])?(?:6|5)?(?:aug|d[io]m|ø|sus4)?7?\*?(?:\(\*\))?)/
+  PATTERN = /\b([A-G])(b|#)?((?:maj|min|[Mm+°])?(?:6|5)?(?:aug|d[io]m|ø|sus4)?7?\*?(?:\(\*\))?)(?:\/([A-Ga-g])(b|#)?)?/
   # PATTERN = /\b([A-G])(b|#)?((?:maj|min|[Mm+°])?(?:6|5)?(?:aug|d[io]m|ø)?7?\*?(?:\(\*\))?)\b/ # => sharp not recognized
   PITCHES = %w(C C# D D# E F F# G G# A A# B)
   ENHARMONICS = {
@@ -18,23 +18,33 @@ class Chord
     @note = note
     @accidental = options[:accidental]
     @modifiers = options[:modifiers]
+    @bass = options[:bass]
+    @bass_accidental = options[:bass_accidental]
     @accidental = nil if @accidental.to_s.strip.empty?
     @modifiers = nil if @modifiers.to_s.strip.empty?
+    @bass = nil if @bass.to_s.strip.empty?
+    @bass_accidental = nil if @bass_accidental.to_s.strip.empty?
   end
 
-  attr_reader :note, :accidental, :modifiers
+  attr_reader :note, :accidental, :modifiers, :bass, :bass_accidental
 
   def to_s
-    "#{root}#{@modifiers}"
+    if @bass
+      bass_suffix = "/#{@bass}#{@bass_accidental}"
+    end
+    "#{root}#{@modifiers}#{bass_suffix}"
   end
 
   def self.from_text(text)
     match = PATTERN.match(text)
     if match
-      note = match[1]
-      accidental = match[2]
-      modifiers = match[3]
-      Chord.new note, accidental: accidental, modifiers: modifiers
+      Chord.new(
+        match[1],
+        accidental: match[2],
+        modifiers: match[3],
+        bass: match[4],
+        bass_accidental: match[5]
+      )
     end
   end
 
@@ -42,16 +52,35 @@ class Chord
     "#{@note}#{@accidental}"
   end
 
+  def bass_root
+    if @bass
+      "#{@bass}#{@bass_accidental}"
+    end
+  end
+
   def transpose(semi)
     r = PITCHES[(pitch + semi) % PITCHES.size]
-    Chord.from_text "#{r}#{@modifiers}"
+    t = "#{r}#{@modifiers}"
+    if @bass
+      br = PITCHES[(bass_pitch + semi) % PITCHES.size]
+      t << "/#{br}"
+    end
+    Chord.from_text t
   end
 
   def normalized
-    Chord.from_text "#{ENHARMONICS[root] || root}#{@modifiers}"
+    n = "#{ENHARMONICS[root] || root}#{@modifiers}"
+    if @bass
+      n << "/#{Chord.from_text(bass_root.upcase).normalized}"
+    end
+    Chord.from_text n
   end
 
   def pitch
     PITCHES.index normalized.root
+  end
+
+  def bass_pitch
+    @bass && PITCHES.index(normalized.bass_root)
   end
 end
